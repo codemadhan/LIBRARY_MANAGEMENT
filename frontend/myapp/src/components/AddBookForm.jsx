daef811abaa6bd99a3fd6f432fbb364811ac85e6
Madhan Konar <iammadhan2103@gmail.com>
2024-02-22 11:21:00 +0530

    ohh yeah baby

--- frontend/myapp/src/components/AddBookForm.jsx
+++ frontend/myapp/src/components/AddBookForm.jsx
@@ -30,6 +30,7 @@ function AddBookForm({ fetchBooks }) {
         dateOfPublishing: '',
         subject: '',
       });
+      alert('Book added successfully');
     } catch (error) {
       console.error('Error adding book: ', error);
     }
